e37399c41c0abeb4bf60c73fa57ca0ede7a53ded
wangwx9 <wangwx9@asiainfo.com>
2022-10-14 17:48:58 +0800

    springcloud-config

---
+++ cloud-config-center-3344/src/main/java/com/wangwx/cloud/configServerMain3344.java
@@ -0,0 +1,20 @@
+package com.wangwx.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @ClassName configServerMain3344
+ * @Description TODO
+ * @Author wangwx9
+ * @Date 2022/10/14 17:37
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class configServerMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(configServerMain3344.class,args);
+    }
+}
